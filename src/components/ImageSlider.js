3e89fed96cc2005e3fde2e7ff491091892d2b827
Lucas Alves <44375412+jlucas-alves@users.noreply.github.com>
2021-07-03 23:14:15 +0100

    test commit

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -29,7 +29,7 @@ const ImageSlider = ({ slides }) => {
             key={index}
           >
             {index === current && (
-              <img src={slide.image} alt='travel image' className='imagealt' />
+              <img src={slide.image} alt='travel image' className='image' />
             )}
           </div>
         );
